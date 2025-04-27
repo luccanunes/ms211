b0679630ff574f2ccb66b63736eddb9853e1e430
Ana Margarida Diniz Silva Borges <164074075+anamargaridaborges@users.noreply.github.com>
2025-04-27 15:37:04 -0300

    newton guardando em vetores

--- newton.py
+++ newton.py
@@ -1,10 +1,13 @@
-def newton(x0, f, f_linha, show=False, max_iter = 1000):
+def newton(x0, f, f_linha, dados_iteracao=[], x=[], show=False, max_iter = 1000):
     ant = 0
     i = 0
+    dados_iteracao.append(x0)
+    x.append(i)
     while abs(x0-ant)>0.0000001 and i < max_iter:
         if show:
             print(x0)
         ant = x0
         x0 = x0 - f(x0)/f_linha(x0)
         i += 1
-    return x0
+        dados_iteracao.append(x0)
+        x.append(i)
