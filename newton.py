4baef21d19dafb3b6a63dac6af7e285276e1ee36
Lucca Nunes <lucca.nunes@ifood.com.br>
2025-04-27 16:14:36 -0300

    atualiza newton

--- newton.py
+++ newton.py
@@ -1,10 +1,10 @@
-def newton(x0, f, f_linha, show=False, max_iter = 1000):
+def newton(x0, f, df, show=False, max_iter = 1000, eps = 1e-10):
     ant = 0
     i = 0
-    while abs(x0-ant)>0.0000001 and i < max_iter:
+    while abs(x0 - ant) > eps and i < max_iter:
         if show:
             print(x0)
         ant = x0
-        x0 = x0 - f(x0)/f_linha(x0)
+        x0 = x0 - f(x0)/df(x0)
         i += 1
     return x0
