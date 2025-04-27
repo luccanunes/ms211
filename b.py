83bc50f4667f02ec741c7d68cf8a497466a5edc0
Ana Margarida Diniz Silva Borges <164074075+anamargaridaborges@users.noreply.github.com>
2025-04-27 15:33:33 -0300

    b certinha

--- b.py
+++ b.py
@@ -1,23 +1,36 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+from newton import newton
 
-def f(x, b):
-    return b - 1/x
-
+def f(x):
+    return math.pi - 1/x
 
 def derivada_f(x): 
     return 1/(x*x)
 
-def newton(x0):
-    ant = 0
-    while abs(x0-ant)>0.0000001:
-        print(x0)
-        ant = x0
-        x0 = x0 - f(x0, math.pi)/derivada_f(x0)
-    print(x0)
+x = np.linspace(-10, 10, 400)
+y = f(x)
+plt.plot(x, y)
+plt.title('Gráfico de f(x) = pi - 1/x')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.show()
 
-print("Executando Newton com x0 = 0.5")
-newton(0.5)
-print("Executando Newton com x0 = 0.7")
-newton(0.7)
+dados_execucao = []
+x = []
+newton(0.5, f, derivada_f, dados_execucao, x)
+plt.plot(x, dados_execucao)
+plt.title('Execução de Newton com x0 = 0.5')
+plt.xlabel('Número da iteração (k)')
+plt.ylabel('Valor estimado na iteração k (xk)')
+plt.show()
 
-
+dados_execucao = []
+x = []
+newton(0.7, f, derivada_f, dados_execucao, x)
+plt.plot(x, dados_execucao)
+plt.title('Execução de Newton com x0 = 0.7')
+plt.xlabel('Número da iteração (k)')
+plt.ylabel('Valor estimado na iteração k (xk)')
+plt.show()
